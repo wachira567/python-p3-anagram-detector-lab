c789ad5a34028b8f54d508901b7e4055b2649996
+ <+>
2025-11-18 13:57:38 +0300

    solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        sorted_word = sorted(self.word)
+        return [w for w in words if sorted(w) == sorted_word]
